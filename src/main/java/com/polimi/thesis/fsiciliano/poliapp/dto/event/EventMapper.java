bd4388e66c79a3206d49f0450ddf0ee1912428f4
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2022-02-06 17:04:33 +0100

    [DTO] - Removed folder

--- src/main/java/com/polimi/thesis/fsiciliano/poliapp/dto/event/EventMapper.java
+++
@@ -1,7 +0,0 @@
-package com.polimi.thesis.fsiciliano.poliapp.dto.event;
-
-import com.polimi.thesis.fsiciliano.poliapp.model.Event;
-
-public interface EventMapper {
-    EventGetDTO eventToGetDTO(Event source);
-}
