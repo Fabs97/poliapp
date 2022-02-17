d3974e96501e01cb9c4d13e1cd8208a8baef9840
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2022-02-18 00:06:01 +0100

    [Repository] - Added and improved queries homepage events

--- src/main/java/com/polimi/thesis/fsiciliano/poliapp/repository/AdministrativeRepository.java
+++ src/main/java/com/polimi/thesis/fsiciliano/poliapp/repository/AdministrativeRepository.java
@@ -1,7 +1,22 @@
 package com.polimi.thesis.fsiciliano.poliapp.repository;
 
 import com.polimi.thesis.fsiciliano.poliapp.model.Administrative;
+import com.polimi.thesis.fsiciliano.poliapp.model.Event;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AdministrativeRepository extends JpaRepository<Administrative, Long> {
+
+    @Query(
+            value = "select event from Event event " +
+                    "join Administrative administrative on event.administrative.id = administrative.id " +
+                    "where event.student.id = :studentId " +
+                    "order by administrative.dateStart asc"
+    )
+    List<Event> findHighlights(@Param("studentId") Long studentId,
+                                    Pageable pageable);
 }
--- src/main/java/com/polimi/thesis/fsiciliano/poliapp/repository/NewsRepository.java
+++ src/main/java/com/polimi/thesis/fsiciliano/poliapp/repository/NewsRepository.java
@@ -2,6 +2,7 @@ package com.polimi.thesis.fsiciliano.poliapp.repository;
 
 import com.polimi.thesis.fsiciliano.poliapp.model.Event;
 import com.polimi.thesis.fsiciliano.poliapp.model.News;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,7 +15,8 @@ public interface NewsRepository extends JpaRepository<News, Long> {
             value = "select event from Event event " +
                     "join News news on event.news.id = news.id " +
                     "where event.student.id = :studentId " +
-                    "order by event.dateStart asc"
+                    "order by news.dateStart asc"
     )
-    List<Event> findNewsOrderByDateAscBy(@Param("studentId") Long studentId);
+    List<Event> findNewsOrderByDateAscBy(@Param("studentId") Long studentId,
+                                         Pageable pageable);
 }
