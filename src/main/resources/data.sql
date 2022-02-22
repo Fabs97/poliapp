11975759d095db8fce1e4b8092bd1e8b7f1d4ca7
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2022-02-22 18:49:54 +0100

    [Data] - Initialized seeds

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO CALENDAR (id, calendar_identifier, calendar_value)VALUES (1, 'custom', 'Calendario personale');
+INSERT INTO CALENDAR (id, calendar_identifier, calendar_value) VALUES (2, 'academic', 'Calendario accademico');
+INSERT INTO CALENDAR (id, calendar_identifier, calendar_value) VALUES (3, 'exams', 'Calendario esami');
+
+INSERT INTO EVENT_TYPE (id, event_type_identifier, event_type_value) VALUES (1, 'custom', 'Evento personale');
+INSERT INTO EVENT_TYPE (id, event_type_identifier, event_type_value) VALUES (2, 'lecture', 'Lezione');
+INSERT INTO EVENT_TYPE (id, event_type_identifier, event_type_value) VALUES (3, 'exam', 'Esame');
+INSERT INTO EVENT_TYPE (id, event_type_identifier, event_type_value) VALUES (4, 'news', 'News');
+INSERT INTO EVENT_TYPE (id, event_type_identifier, event_type_value) VALUES (5, 'administrative', 'Scadenza');
+
+INSERT INTO STUDENT (id) VALUES (1);
+INSERT INTO STUDENT (id) VALUES (2);
+INSERT INTO STUDENT (id) VALUES (3);
+INSERT INTO STUDENT (id) VALUES (4);
+INSERT INTO STUDENT (id) VALUES (5);
+INSERT INTO STUDENT (id) VALUES (6);
+INSERT INTO STUDENT (id) VALUES (7);
+
